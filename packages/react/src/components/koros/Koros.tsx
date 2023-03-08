7c8f5fbad4e471cffa890fd61dbebe07d6cc50e1
nlaitine <niko.laitinen@nitor.com>
2023-03-08 13:30:16 +0200

    All svg shapes updated to Koro component

--- packages/react/src/components/koros/Koros.tsx
+++ packages/react/src/components/koros/Koros.tsx
@@ -46,13 +46,13 @@ const getSVG = (dense: boolean, type: string, patternName: string): React.SVGPro
     beat: (
       <path
         transform={transform}
-        d="m0 21v64h32v-64c-4 0-5.4-4-5.4-4l-5.2-13s-1.4-4-5.4-4-5.4 4-5.4 4l-5.2 13s-1.4 4-5.4 4z"
+        d="m0,63v33h96v-33c-12,0-16.2-12-16.2-12l-15.6-39s-4.2-12-16.2-12-16.2,12-16.2,12l-15.6,39s-4.2,12-16.2,12z"
       />
     ),
-    pulse: <path transform={transform} d="m0 10v75h32v-75c-8 0-8-10-16-10s-8 10-16 10z" />,
-    storm: <path transform={transform} d="m0 10v75h32v-75c-8 0-13-3.7-16-10-3 6.3-8 10-16 10z" />,
-    wave: <path transform={transform} d="m0 0v85h32v-85l-16 16z" />,
-    calm: <path transform={transform} d="m0 0v85h32v-85z" />,
+    pulse: <path transform={transform} d="m0,30v66h96v-66c-24,0-24-30-48-30s-24,30-48,30z" />,
+    storm: <path transform={transform} d="m0,30v66h96v-66c-24,0-39-11.1-48-30-9,18.9-24,30-48,30z" />,
+    wave: <polygon transform={transform} points="m0,0v96h96v-96l-48,48z" />,
+    calm: <path transform={transform} d="m0,0v96h96v-96z" />,
   };
 
   return (
