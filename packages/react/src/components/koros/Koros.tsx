1d79dbab3ecb50a636b8880ec635031805a18c4f
nlaitine <niko.laitinen@nitor.com>
2023-05-29 16:18:50 +0300

    Koro snapshot tests updated

--- packages/react/src/components/koros/Koros.tsx
+++ packages/react/src/components/koros/Koros.tsx
@@ -50,8 +50,8 @@ const getSVG = (dense: boolean, type: string, patternName: string): React.SVGPro
       />
     ),
     pulse: <path transform={transform} d="m0 10v75h32v-75c-8 0-8-10-16-10s-8 10-16 10z" />,
-    storm: <path transform={transform} d="m0 10v75h32v-75c-8 0-13-3.7-16-10-3 6.3-8 10-16 10z" />,
-    wave: <path transform={transform} d="m0 0v85h32v-85l-16 16z" />,
+    wave: <path transform={transform} d="m0 10v75h32v-75c-8 0-13-3.7-16-10-3 6.3-8 10-16 10z" />,
+    vibration: <path transform={transform} d="m0 0v85h32v-85l-16 16z" />,
     calm: <path transform={transform} d="m0 0v85h32v-85z" />,
   };
 
